0960f71763aa0b9b9cc08eeea34a0158833923b1
edogola4 <edogola4@gmail.com>
2025-03-17 19:03:31 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -10,7 +10,7 @@ ipdb.set_trace()
 """
 #!/usr/bin/env python3
 #lib/testing/debug.py
-
+"""
 from __init__ import CONN, CURSOR
 from department import Department
 
@@ -34,4 +34,25 @@ print("Delete Payroll")
 payroll.delete()  # delete from db table, object still exists in memory
 print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
 
+ipdb.set_trace()
+"""
+
+#!/usr/bin/env python3
+
+from __init__ import CONN, CURSOR
+from department import Department
+
+import ipdb
+
+
+def reset_database():
+    Department.drop_table()
+    Department.create_table()
+
+    Department.create("Payroll", "Building A, 5th Floor")
+    Department.create("Human Resources", "Building C, East Wing")
+    Department.create("Accounting", "Building B, 1st Floor")
+
+
+reset_database()
 ipdb.set_trace()
